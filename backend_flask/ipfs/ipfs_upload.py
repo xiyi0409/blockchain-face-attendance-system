a6e028ac24b9a35b626860831545c68cb058bf34
xiyi0409 <allenzhao1001@gmail.com>
2025-12-01 11:34:46 +0800

    Update ipfs_upload.py

--- backend_flask/ipfs/ipfs_upload.py
+++ backend_flask/ipfs/ipfs_upload.py
@@ -1,6 +1,18 @@
 import ipfshttpclient
 
-def upload_to_ipfs(file_path):
-    client = ipfshttpclient.connect("/ip4/127.0.0.1/tcp/5001")  
-    result = client.add(file_path)
-    return result["Hash"]
+def upload_to_ipfs(file_obj):
+    """
+    將上傳的圖片檔 (Werkzeug FileStorage) 直接傳到 IPFS，
+    回傳 CID (字串)。
+    """
+    # 連線到本機 IPFS 節點 (Kubo)
+    client = ipfshttpclient.connect("/ip4/127.0.0.1/tcp/5001")
+
+    # 讀取檔案內容成 bytes
+    data = file_obj.read()
+
+    # 上傳 bytes，取得 CID
+    cid = client.add_bytes(data)
+
+    # 如果之後還要重複讀檔，可以 file_obj.seek(0)
+    return cid
